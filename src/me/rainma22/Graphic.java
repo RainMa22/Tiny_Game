bc9ab6411576fd7b7160413c2da25188666fb7ab
rainma22 <d2rain@126.com>
2020-09-13 21:09:56 -0600

    ImageResize added

--- src/me/rainma22/Graphic.java
+++ src/me/rainma22/Graphic.java
@@ -2,7 +2,11 @@ package me.rainma22;
 import javax.swing.*;
 import java.awt.Graphics;
 public class Graphic extends JPanel{
+    private container c;
+    public Graphic(container c){
+        this.c=c;
+    }
     public void drawComponents(Graphics g){
-        
+
     }
 }
--- src/me/rainma22/Main.java
+++ src/me/rainma22/Main.java
@@ -1,18 +1,52 @@
 package me.rainma22;
+import me.rainma22.utils.ImageResize;
+import me.rainma22.utils.coords;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-public class Main {
-    public static void main(String[] args) {
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
 	container c=new container();
     }
-    class container{
-        public BufferedImage player= new BufferedImage(3,2,BufferedImage.TYPE_INT_RGB);
-        public BufferedImage bullet= new BufferedImage(1,1,BufferedImage.TYPE_INT_RGB);
-        public container(){
-            player.setRGB(2,1,new Color(255,255,255).getRGB());
-            player.setRGB(1,2,new Color(255,255,255).getRGB());
-            player.setRGB(3,2,new Color(255,255,255).getRGB());
-            bullet.setRGB(1,1,new Color(255,255,255).getRGB());
-        }
+
+}
+class container{
+    public JFrame frame=new JFrame("Tiny_Game");
+    private Dimension avaliableSpace=Toolkit.getDefaultToolkit().getScreenSize();
+    public BufferedImage player= new BufferedImage(3,2,BufferedImage.TYPE_INT_ARGB);
+    public coords playerCord;
+    public BufferedImage bullet= new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
+    public ArrayList<coords> bulletCords=new ArrayList<>(0);
+    public BufferedImage enemy=new BufferedImage(3,2,BufferedImage.TYPE_INT_ARGB);
+    public ArrayList<coords> enemyCords=new ArrayList<>(0);
+    public BufferedImage ebullet= new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
+    public ArrayList<coords> ebulletCords=new ArrayList<>(0);
+    public container() throws IOException {
+        player.setRGB(1,0,new Color(0, 0, 0).getRGB());
+        player.setRGB(0,1,new Color(0,0,0).getRGB());
+        player.setRGB(1,1,new Color(0,0,0).getRGB());
+        player.setRGB(2,1,new Color(0,0,0).getRGB());
+        enemy.setRGB(1,0,new Color(255,0,0).getRGB());
+        enemy.setRGB(0,0,new Color(255,0,0).getRGB());
+        enemy.setRGB(1,1,new Color(255,0,0).getRGB());
+        enemy.setRGB(2,0,new Color(255,0,0).getRGB());
+        bullet.setRGB(0,0,new Color(0,0,0).getRGB());
+        ebullet.setRGB(0,0,new Color(255,0,0).getRGB());
+        frame.setBounds(avaliableSpace.width/2-avaliableSpace.width/5,0,(avaliableSpace.width/5)*2,avaliableSpace.height);
+        player= ImageResize.Scaled(player,frame.getWidth()/10,frame.getWidth()/15);
+        enemy=ImageResize.Scaled(enemy,frame.getWidth()/10,frame.getWidth()/15);
+        bullet= ImageResize.Scaled(bullet,frame.getWidth()/30,frame.getWidth()/30);
+        ebullet= ImageResize.Scaled(ebullet,frame.getWidth()/30,frame.getWidth()/30);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
